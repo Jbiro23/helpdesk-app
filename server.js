50765b1495fae2df341e78f8f56548d10f783c51
bRoxorg <janbiro2003@gmail.com>
2026-08-31 21:11:53 +0200

    Use environment PORT for deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const ticketRoutes = require("./routes/tickets");
 const knowledgeRoutes = require("./routes/knowledge");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
